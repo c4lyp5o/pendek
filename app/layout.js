4d697341b60beef9521bffd2fdeb8c722c6bac1e
c4lyp5o <calypso@calypsocloud.one>
2023-12-13 15:45:57 +0800

    fix: tambah toastcontainer untuk kegunaan di layout

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
 import { Inter } from 'next/font/google';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -18,6 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={inter.className}>{children}</body>
+      <ToastContainer />
     </html>
   );
 }
